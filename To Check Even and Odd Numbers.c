93f0b386991d4bc24f833e11a482553020bba777
smrutigurav <guravsmruti2@gmail.com>
2025-04-30 17:54:02 +0530

    Even and odd number

---
+++ To Check Even and Odd Numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int num1;
+	printf("Enter The Number :");
+	scanf("%d",&num1);
+	if(num1%2==0)
+	{
+		printf("\nNumber is Even");
+	}
+	else
+	{
+		printf("\nNumber is Odd");
+	}
+	return 0;
+}
